5b2eb368b1c4e9be7f45822a2a7cb6d57a0a3b79
jose Angel <josegallardo.cor@gmail.com>
2025-04-14 23:51:22 -0600

    Asignar roles en el registro

--- EcomerceRazorPages/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EcomerceRazorPages/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 using ECommerce.Models;
+using ECommerce.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -30,13 +31,15 @@ namespace ECommerceRazorPages.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +47,7 @@ namespace ECommerceRazorPages.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager; 
         }
 
         /// <summary>
@@ -140,8 +144,28 @@ namespace ECommerceRazorPages.Areas.Identity.Pages.Account
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+                //manejo de roles
+                if (!await _roleManager.RoleExistsAsync(CNT.Administrador))
+                {//crea los role por primera ves 
+                    _roleManager.CreateAsync(new IdentityRole(CNT.Administrador)).GetAwaiter().GetResult();
+                    _roleManager.CreateAsync(new IdentityRole(CNT.Cliente)).GetAwaiter().GetResult();
+                }
+
                 if (result.Succeeded)
                 {
+                    string role = Request.Form["radioUserRole"].ToString();
+                    if(role == CNT.Administrador)
+                    {//permite asignar un role al usuario
+                        await _userManager.AddToRoleAsync(user, CNT.Administrador);
+                    }
+                    else
+                    {
+                        if (role == CNT.Cliente)
+                        {
+                            await _userManager.AddToRoleAsync(user, CNT.Cliente);
+                        }
+                    }
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
